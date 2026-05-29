e4450a6d27342a8a364a37dff49ce5de7f6fb45c
BhavyaKarnati02 <karnatibhavya1107@gmail.com>
2026-05-29 17:17:56 +0530

    fixed

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-def signup(request):
-
-    if request.method == "POST":
-
+def register(request):
+    if request.method == 'POST':
         form = UserCreationForm(request.POST)
 
         if form.is_valid():
-
-            form.save()
-
-            return redirect("login")
+            user = form.save()
+            login(request, user)
+            return redirect('home')
 
     else:
-
         form = UserCreationForm()
 
-    return render(request, "registration/signup.html", {
-        "form": form
-    })
+    return render(request, 'registration/register.html', {'form': form})
